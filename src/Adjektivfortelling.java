d9c4dbe3d38657c3ed0da77325eb65fb28b6fea7
eirinko <eirinkorvald.ek@gmail.com>
2022-06-07 20:07:35 +0200

    Trying to create madlibs where you can use your own story and add adjectives in the terminal (user input for file and adjectives).

--- src/Adjektivfortelling.java
+++ src/Adjektivfortelling.java
@@ -7,17 +7,17 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 // Next time: sjekke antall adjektiv i adjektivfortellingen og deretter sørge for at det er så mange eller flere adjektiv å velge fra.
 public class Adjektivfortelling {
-
 	public static void main(String[] args) 
 		throws IOException
 	{
 		Scanner scan = new Scanner(System.in);
 		//Resource leak: 'scan' is never closed. Should it be closed? 
 		scan.useDelimiter(Pattern.compile("[\\n;]+"));
+
+
 		System.out.println("Skriv inn adjektivene dine. Avslutt med ENTER.");
 		String adjektiv = scan.next();
 		String[] splittet = adjektiv.split("\\s+");
@@ -35,17 +35,28 @@ public class Adjektivfortelling {
 		}
 		
 		// Creating a path choosing file from local directory by creating an object of Path class
-		String filepos = scan.next();
+		
 		System.out.println("Write path and file to use and then ENTER: ");
+		String filepos = scan.next();
 		filepos = filepos.substring(0, filepos.length()-1);
         Path fortelling = Path.of(filepos);
 
         String str = Files.readString(fortelling);
-        int count = 0;
+        
+        String strFind = "[adjektiv]";
+        int count = 0, fromIndex = 0;
+        
+        while ((fromIndex = str.indexOf(strFind, fromIndex)) != -1 ){
+            count++;
+            fromIndex++;
+        }
+		
+		System.out.println(count);
+        int countadj = 0;
         
         while (str.contains("[adjektiv]")) {
-        	str = str.replaceFirst("\\[adjektiv\\]", utvalgteAdjektiv[count]);
-        	count++;        
+        	str = str.replaceFirst("\\[adjektiv\\]", utvalgteAdjektiv[countadj]);
+        	countadj++;
         }
         System.out.println(str);
 	}
